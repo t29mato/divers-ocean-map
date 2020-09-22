2d3085433f657fc7d397dcdec0d34618cae76dfe
t29mato <tomoya.matou@gmail.com>
2020-09-22 13:07:37 +0900

    test: add test data

--- hello-world/service/scraping_izu_ocean_park_test.go
+++ hello-world/service/scraping_izu_ocean_park_test.go
@@ -30,4 +30,22 @@ func TestScrapeIOP(t *testing.T) {
 		},
 		MeasuredTime: time.Date(2020, time.September, 13, 0, 0, 0, 0, time.UTC),
 	}, ocean)
+
+	url = pwd + "/testdata/" + t.Name() + "_20200922.html"
+	s.ScrapingService.url = url
+	ocean, _ = s.Scrape()
+	assert.Equal(t, &model.Ocean{
+		LocationName: "伊豆海洋公園",
+		Temperature: model.Temperature{
+			Min: 23,
+			Med: -1,
+			Max: 26,
+		},
+		Visibility: model.Visibility{
+			Min: -1,
+			Med: 15,
+			Max: -1,
+		},
+		MeasuredTime: time.Date(2020, time.September, 22, 0, 0, 0, 0, time.UTC),
+	}, ocean)
 }
