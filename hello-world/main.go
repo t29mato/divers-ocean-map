5afaf903d93ff27b57fd6888233fae12feccc820
t29mato <tomoya.matou@gmail.com>
2020-09-22 13:21:00 +0900

    refactor: コメント修正

--- hello-world/main.go
+++ hello-world/main.go
@@ -6,25 +6,26 @@ import (
 )
 
 func main() {
+	// TODO: goroutine 使う
 	scrapingServiceIzuOceanPark := service.NewScrapingServiceIzuOceanPark()
 	oceanIzuOceanPark, err := scrapingServiceIzuOceanPark.Scrape()
 	if err != nil {
-		fmt.Println("スクレイピングの途中で失敗しました", err)
+		fmt.Println("伊豆海洋公園のスクレイピングの途中で失敗しました", err)
 	}
 
 	err = scrapingServiceIzuOceanPark.ScrapingService.Store(oceanIzuOceanPark)
 	if err != nil {
-		fmt.Println("DBへの挿入で失敗", err)
+		fmt.Println("伊豆海洋公園のDBへの挿入で失敗", err)
 	}
 
 	scrapingServiceUkishimaTiba := service.NewScrapingServiceUkishimaTiba()
 	oceanUkishimaTiba, err := scrapingServiceUkishimaTiba.Scrape()
 	if err != nil {
-		fmt.Println("スクレイピングの途中で失敗しました", err)
+		fmt.Println("浮島 (千葉県勝山市)のスクレイピングの途中で失敗しました", err)
 	}
 
 	err = scrapingServiceUkishimaTiba.ScrapingService.Store(oceanUkishimaTiba)
 	if err != nil {
-		fmt.Println("DBへの挿入で失敗", err)
+		fmt.Println("浮島 (千葉県勝山市)のDBへの挿入で失敗", err)
 	}
 }
