e19b9974749022e2d2f264cc15d6f64fa4195bce
t29mato <tomoya.matou@gmail.com>
2020-10-04 02:41:09 +0900

    refactor: package分割

--- functions/scraping/service/scraping.go
+++ functions/scraping/service/scraping.go
@@ -1,29 +1,10 @@
 package service
 
 import (
-	"scraping/logging"
 	"scraping/model"
 )
 
 // ScrapingService ...
 type ScrapingService interface {
 	Scrape() (*model.Ocean, error)
-}
-
-// ScrapingServiceImpl ...
-type ScrapingServiceImpl struct {
-	logging *logging.OceanLoggingImpl
-	db      *DynamoDBServiceImpl
-}
-
-// Store ...
-func (s *ScrapingServiceImpl) Store(ocean *model.Ocean) error {
-	s.logging.Info("データの永久保存開始", ocean.LocationName)
-	err := s.db.CreateIfNotExist(ocean)
-	if err != nil {
-		s.logging.Info("データの永久保存に失敗")
-		return err
-	}
-	s.logging.Info("データの永久保存終了", ocean.LocationName)
-	return nil
 }
